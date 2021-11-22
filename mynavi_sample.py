f65e4353adf0db9af9617d8b8873efcb693d2b6f
takashi002013@gmail.com <takashi002013@gmail.com>
2021-11-22 17:59:57 +0900

    1st-commit

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -1,76 +1,92 @@
 import os
 from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
 
-# Chromeを起動する関数
+# Selenium4対応済
 
 
-def set_driver(driver_path, headless_flg):
-    if "chrome" in driver_path:
-          options = ChromeOptions()
-    else:
-      options = Options()
+def set_driver(hidden_chrome: bool=False):
+    '''
+    Chromeを自動操作するためのChromeDriverを起動してobjectを取得する
+    '''
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    options = ChromeOptions()
 
     # ヘッドレスモード（画面非表示モード）をの設定
-    if headless_flg == True:
+    if hidden_chrome:
         options.add_argument('--headless')
 
     # 起動オプションの設定
-    options.add_argument(
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
-    # options.add_argument('log-level=3')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('--incognito')          # シークレットモードの設定を付与
-
+    options.add_argument(f'--user-agent={USER_AGENT}') # ブラウザの種類を特定するための文字列
+    options.add_argument('log-level=3') # 不要なログを非表示にする
+    options.add_argument('--ignore-certificate-errors') # 不要なログを非表示にする
+    options.add_argument('--ignore-ssl-errors') # 不要なログを非表示にする
+    options.add_experimental_option('excludeSwitches', ['enable-logging']) # 不要なログを非表示にする
+    options.add_argument('--incognito') # シークレットモードの設定を付与
+    
     # ChromeのWebDriverオブジェクトを作成する。
-    if "chrome" in driver_path:
-        return Chrome(executable_path=os.getcwd() + "/" + driver_path,options=options)
-    else:
-        return Firefox(executable_path=os.getcwd()  + "/" + driver_path,options=options)
-
-# main処理
+    service=Service(ChromeDriverManager().install())
+    return Chrome(service=service, options=options)
 
 
 def main():
+    '''
+    main処理
+    '''
     search_keyword = "高収入"
     # driverを起動
-    if os.name == 'nt': #Windows
-        driver = set_driver("chromedriver.exe", False)
-    elif os.name == 'posix': #Mac
-        driver = set_driver("chromedriver", False)
+    driver = set_driver()
+    
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
     time.sleep(5)
-    # ポップアップを閉じる
+    
+    '''
+    ポップアップを閉じる
+    ※余計なポップアップが操作の邪魔になる場合がある
+      モーダルのようなポップアップ画面は、通常のclick操作では処理できない場合があるため
+      excute_scriptで直接Javascriptを実行することで対処する
+    '''
     driver.execute_script('document.querySelector(".karte-close").click()')
     time.sleep(5)
     # ポップアップを閉じる
     driver.execute_script('document.querySelector(".karte-close").click()')
 
-    # 検索窓に入力
-    driver.find_element_by_class_name(
-        "topSearch__text").send_keys(search_keyword)
-    # 検索ボタンクリック
-    driver.find_element_by_class_name("topSearch__button").click()
 
-    # ページ終了まで繰り返し取得
-    # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    '''
+    find_elementでHTML要素(WebElement)を取得する
+    byで、要素を特定する属性を指定するBy.CLASS_NAMEの他、By.NAME、By.ID、By.CSS_SELECTORなどがある
+    特定した要素に対して、send_keysで入力、clickでクリック、textでデータ取得が可能
+    '''
+    # 検索窓に入力
+    driver.find_element(by=By.CLASS_NAME, value="topSearch__text").send_keys(search_keyword)
+    # 検索ボタンクリック
+    driver.find_element(by=By.CLASS_NAME, value="topSearch__button").click()
+
+
+    '''
+    find_elements(※複数形)を使用すると複数のデータがListで取得できる
+    一覧から同一条件で複数のデータを取得する場合は、こちらを使用する
+    '''
+    name_elms = driver.find_elements(by=By.CLASS_NAME, value="cassetteRecruit__name")
     
     # 空のDataFrame作成
     df = pd.DataFrame()
 
     # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        print(name.text)
+    print(len(name_elms))
+    '''
+    name_elmsには１ページ分の情報が格納されているのでforでループさせて１つづつ取り出して、Dataframeに格納する
+    '''
+    for name_elm in name_elms:
+        print(name_elm.text)
         # DataFrameに対して辞書形式でデータを追加する
         df = df.append(
-            {"会社名": name.text, 
+            {"会社名": name_elm.text, 
              "項目B": "",
              "項目C": ""}, 
             ignore_index=True)
